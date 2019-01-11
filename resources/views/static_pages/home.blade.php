8bebc13fdad4216395830a5acbd3dab04fbf291b
lily_deng <912617726@qq.com>
2019-01-11 11:59:07 +0800

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是<a href="https://laravel-china.org/courses/laravel0essential-training-5.5">Laravel 入门教程</a>
+        </p>
+        <p>一起，将从这里开始。</p>
+        <p><a href="btn btn-lg btn-succcess" href="{{ route('signup')}}" role="button">现在注册</a>
+    </div>
 @stop
