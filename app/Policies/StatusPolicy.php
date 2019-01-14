35de191aac075ebca5121b646207ced75aeb234e
lily_deng <912617726@qq.com>
2019-01-14 17:10:48 +0800

    Add user statuses

--- app/Policies/StatusPolicy.php
+++ app/Policies/StatusPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\Status;
 
 class StatusPolicy
 {
@@ -17,5 +18,10 @@ class StatusPolicy
     public function __construct()
     {
         //
+    }
+
+    public function destroy(User $user, Status $status)
+    {
+        return $user->id === $status->user_id;
     }
 }
